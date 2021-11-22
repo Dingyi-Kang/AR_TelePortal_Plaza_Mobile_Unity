fd23d8efd750e347ac781009c0a8665db631240c
Dingyi <dkang@okstate.edu>
2021-11-21 21:29:15 -0600

    init commit

---
+++ Assets/Imported/AddOns/CodeMonkey/KeyDoorSystem/Scripts/DoorLock.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorLock : MonoBehaviour
+{
+
+    Animator m_Animator;
+    int isOpenHash;
+    bool isClosed = true;
+
+    void Awake()
+    {
+        // Cache Animator Component
+        m_Animator = GetComponent<Animator>();
+        isOpenHash = Animator.StringToHash("open");
+    }
+
+    public void Update()
+    {
+        // Play Open Door Animation
+        //potential issue: touch too long time, and update too quick
+        //this is due to one button tap and unpate func
+        if (Input.GetKey("o"))
+        {
+
+            if (isClosed)
+            {
+                //open it
+                m_Animator.SetBool(isOpenHash, true);
+                isClosed = false;
+
+            }
+            else
+            {
+                //close it
+                m_Animator.SetBool(isOpenHash, false);
+                isClosed = true;
+            }
+
+        }
+
+    }
+
+}
